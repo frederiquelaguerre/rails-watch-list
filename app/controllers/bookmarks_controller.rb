7dc79b74d139ffd135e7a61898fbd80cfc44bf3d
Frédérique Laguerre <laguerre.frederique@gmail.com>
2023-11-24 19:18:26 +0100

    fin dd'exo

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -16,10 +16,9 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
-    @list = List.find(params[:list_id])
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to list_path(@list), status: :see_other, notice: 'Bookmark was successfully destroyed.'
+    redirect_to list_path(@bookmark.list), status: :see_other, notice: 'Bookmark was successfully destroyed.'
   end
 
   private
